04e6e5f458fb6c3bb0aa6eadb5eb2ed7a7cb7334
Ukraine1449 <roma.bystriakov@gmail.com>
2021-05-03 21:58:33 -0400

    Created join and leave messages

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
 import discord
 from discord.ext import commands
-client = commands.Bot(command_prefix = ".")
+
+intents = discord.Intents.default()
+intents.members = True
+intents.presences = True
+
+client = commands.Bot(command_prefix=".", intents=intents)
+
 
 @client.event
 async def on_ready():
     print('Startup succesful')
+
+
+@client.event
+async def on_member_join(member):
+    print(f'{member} has joined the server')
+    channel = client.get_channel(838951796386168866)
+    await channel.send(f'{member} has joined.')
+
+@client.event
+async def on_member_remove(member):
+    print(f'{member} has left the server')
+    channel = client.get_channel(838951796386168866)
+    await channel.send(f'{member} has lefts.')
+
 
 client.run('ODM4OTAyOTMzOTg1Njg5NjEw.YJB3PQ.KLqjsVUQkiobUxxesHiziXQ4h0I')
