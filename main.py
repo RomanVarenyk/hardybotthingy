1f7753af304af41b4df8faf554b393fc6ab131ff
Ukraine1449 <roma.bystriakov@gmail.com>
2021-05-03 22:13:25 -0400

    Created join and leave messages

--- main.py
+++ main.py
@@ -18,12 +18,15 @@ async def on_member_join(member):
     print(f'{member} has joined the server')
     channel = client.get_channel(838951796386168866)
     await channel.send(f'{member} has joined.')
-
+Working on ban event
 @client.event
 async def on_member_remove(member):
     print(f'{member} has left the server')
     channel = client.get_channel(838951796386168866)
     await channel.send(f'{member} has lefts.')
 
+@client.event
+async def on_member_ban(member):
+    print(f'{member} has been banned by ')
 
 client.run('ODM4OTAyOTMzOTg1Njg5NjEw.YJB3PQ.KLqjsVUQkiobUxxesHiziXQ4h0I')
