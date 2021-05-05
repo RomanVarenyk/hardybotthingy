da3b8f13483ca2c5b8d89c0e2798f69d57d8ad8b
Ukraine1449 <roma.bystriakov@gmail.com>
2021-05-05 16:01:16 -0400

    Kick command

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ from discord.ext import commands
 intents = discord.Intents.default()
 intents.members = True
 intents.presences = True
-
+intents.messages = True
+badwords = ['badwordtest']
 client = commands.Bot(command_prefix=".", intents=intents)
 
 
@@ -36,6 +37,15 @@ async def on_member_ban(guild, member):
         print(f'{logs.user} has just banned {logs.target} (Time: {logs.created_at}), reason was: {logs.reason}')
 
 
+@client.event
+async def on_message(checkmessage):
+    if checkmessage.content in badwords:
+        print(f'{checkmessage.content} from {checkmessage.author} has beed deleted',
+              f' Full string of message: {checkmessage}')
+        await checkmessage.delete()
+    await client.process_commands(checkmessage)
+
+
 @client.command()
 @commands.has_role('Approved')
 async def pd(ctx, member: discord.Member = None):
@@ -47,10 +57,23 @@ async def pd(ctx, member: discord.Member = None):
     print(f"Successfully gave {member} the {role.name} role!")
 
 
+@client.command()
+async def clear(ctx, ammount=5):
+    await ctx.channel.purge(limit=ammount)
+
 
 @client.command()
 async def ping(ctx):
     await ctx.send('The ping is ' + str(round(client.latency, 7)))
     print('The ping is ' + str(round(client.latency, 7)))
+
+
+
+
+@client.command()
+async def kick(ctx, member: discord.Member, *, reason=None):
+    await member.kick(reason=reason)
+    await member.send(f'You were kicked from hardy ms discord for {reason}')
+
 
 client.run('ODM4OTAyOTMzOTg1Njg5NjEw.YJB3PQ.Y7axtdIsXn9zHEaCITirZIfX_XE')
