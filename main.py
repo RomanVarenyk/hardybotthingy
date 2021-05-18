a36a77881b7f1b41642ca3d5b331ff6eb8571c74
Ukraine1449 <roma.bystriakov@gmail.com>
2021-05-17 20:37:09 -0400

    Added logging

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands, tasks
+from datetime import datetime
 
 intents = discord.Intents.default()
 intents.members = True
@@ -9,15 +10,25 @@ badwords = ['fag', 'faggot', 'nigga', 'nigger']
 client = commands.Bot(command_prefix=".", intents=intents)
 
 
+now = str(datetime.now().strftime("%m/%d/%Y, %H:%M:%S"))
+now = now.replace("/", ".")
+now = now.replace(":", ".")
+file = open(f'Logs from ' + str(now) + ".txt", "a")
 @client.event
 async def on_ready():
     await client.change_presence(status=discord.Status.online, activity=discord.Game('no idea what to put here so this is here'))
     print('Startup succesful')
+    filee = open(f'Logs from ' + str(now) + ".txt", "a")
+    filee.write('Startup succesful')
+    filee.close()
 
 
 @client.event
 async def on_member_join(member):
     print(f'{member} has joined the server')
+    filee = open(f'Logs from ' + str(now) + ".txt", "a")
+    filee.write(f'\n {member} has joined the server')
+    filee.close()
     channel = client.get_channel(708316172310937652)
     await channel.send(f'{member} has joined.')
 
@@ -25,6 +36,9 @@ async def on_member_join(member):
 @client.event
 async def on_member_remove(member):
     print(f'{member} has left the server')
+    filee = open(f'Logs from ' + str(now) + ".txt", "a")
+    filee.write(f'\n {member} has left the server')
+    filee.close()
     channel = client.get_channel(708316172310937652)
     await channel.send(f'{member} has left.')
 
@@ -34,6 +48,9 @@ async def on_member_ban(guild, member):
     logs = await guild.audit_logs(limit=1, action=discord.AuditLogAction.ban).flatten()
     logs = logs[0]
     if logs.target == member:
+        filee = open(f'Logs from ' + str(now) + ".txt", "a")
+        filee.write(f'\n {logs.user} has just banned {logs.target} (Time: {logs.created_at}), reason was: {logs.reason}')
+        filee.close()
         print(f'{logs.user} has just banned {logs.target} (Time: {logs.created_at}), reason was: {logs.reason}')
 
 
@@ -42,6 +59,10 @@ async def on_message(checkmessage):
     if checkmessage.content in badwords:
         print(f'{checkmessage.content} from {checkmessage.author} has beed deleted',
               f' Full string of message: {checkmessage}')
+        filee = open(f'Logs from ' + str(now) + ".txt", "a")
+        filee.write(f'\n {checkmessage.content} from {checkmessage.author} has beed deleted',
+              f' Full string of message: {checkmessage}')
+        filee.close()
         await checkmessage.delete()
     await client.process_commands(checkmessage)
 
@@ -54,18 +75,28 @@ async def pd(ctx, member: discord.Member = None):
     role = ctx.guild.get_role(819652594245828628)  # role ID goes there
     await member.add_roles(role)
     await ctx.send(f"Successfully gave {member} the {role.name} role!")
+    filee = open(f'Logs from ' + str(now) + ".txt", "a")
+    filee.write(f"\n Successfully gave {member} the {role.name} role!")
+    filee.close()
     print(f"Successfully gave {member} the {role.name} role!")
 
 
 @client.command()
 async def clear(ctx, ammount=5):
     await ctx.channel.purge(limit=ammount)
+    print(f'just purged {ammount} messages from {ctx.channel}             full string {ctx}')
+    filee = open(f'Logs from ' + str(now) + ".txt", "a")
+    filee.write(f'\n just purged {ammount} messages from {ctx.channel}             full string {ctx}')
+    filee.close()
 
 
 @client.command()
 async def ping(ctx):
     await ctx.send('The ping is ' + str(round(client.latency, 7)))
     print('The ping is ' + str(round(client.latency, 7)))
+    filee = open(f'Logs from ' + str(now) + ".txt", "a")
+    filee.write('\n The ping is ' + str(round(client.latency, 7)))
+    filee.close()
 
 
 @client.command()
@@ -73,6 +104,9 @@ async def ping(ctx):
 async def kick(ctx, member: discord.Member, *, reason=None):
     await member.send(f'You were kicked from hardy ms discord for {reason}')
     print(f'{member} was kicked for: {reason}')
+    filee = open(f'Logs from ' + str(now) + ".txt", "a")
+    filee.write(f'\n {member} was kicked for: {reason}')
+    filee.close()
     await member.kick(reason=reason)
 
 
@@ -82,6 +116,9 @@ async def kick(ctx, member: discord.Member, *, reason=None):
 async def ban(ctx, member: discord.Member, *, reason=None):
     await member.send(f'You were ban from hardy ms discord for {reason}')
     print(f'{member} was banned for: {reason}')
+    filee = open(f'Logs from ' + str(now) + ".txt", "a")
+    filee.write(f'\n {member} was banned for: {reason}')
+    filee.close()
     await member.ban(reason=reason)
 
 
@@ -89,6 +126,9 @@ async def ban(ctx, member: discord.Member, *, reason=None):
 @commands.has_role('Approved')
 async def suggest(ctx, member, *, suggestions):
     print(f'{ctx.author} suggested: {suggestions}')
+    filee = open(f'Logs from ' + str(now) + ".txt", "a")
+    filee.write(f'\n {ctx.author} suggested: {suggestions}')
+    filee.close()
     channel = client.get_channel(709569397731360822)
     await channel.send(f'{ctx.author} suggested: {suggestions}')
 
@@ -105,6 +145,9 @@ async def unban(ctx, *, memberid):
         if user.id == memberidint:
             await ctx.guild.unban(user)
             print(f'{memberidint} was unbanned by {ctx.author}')
+            filee = open(f'Logs from ' + str(now) + ".txt", "a")
+            filee.write(f'\n {memberidint} was unbanned by {ctx.author}')
+            filee.close()
             channel = client.get_channel(709569397731360822)
             await channel.send(f'{memberidint} was unbanned by {ctx.author}')
             break
@@ -114,9 +157,14 @@ async def unban(ctx, *, memberid):
 @client.event
 async def on_message_delete(message):
     print(f'{message.content} was deleted. Full string: {message}')
-
+    filee = open(f'Logs from ' + str(now) + ".txt", "a")
+    filee.write(f'\n {message.content} was deleted. Full string: {message}')
+    filee.close()
 @client.event
 async def on_message_edit(before, after):
+    filee = open(f'Logs from ' + str(now) + ".txt", "a")
+    filee.write(f'\n {before.author} has edit the messaage from {before.content} to {after.content}. The  full string of before and after. {before} ........... and after {after}')
+    filee.close()
     print(f'{before.author} has edit the messaage from {before.content} to {after.content}. The  full string of before and after. {before} ........... and after {after}')
 
 
